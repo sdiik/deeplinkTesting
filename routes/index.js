853aa7f114b419f7b722b652a6e9dfd93e42b0ce
asani <asani@asanis-MacBook-Pro-2.local>
2024-09-05 10:36:12 +0700

    update code node js

---
+++ routes/index.js
@@ -0,0 +1,37 @@
+const router = require('express').Router();
+// const apiRouter = require('./api.js');
+//for handling the GET request on 'localhost:2345/'
+/*
+	localhost:2345/ is same as writing localhost:2345. The browser automatically routerends a "/" if there isn't any
+*/
+router.get('/', function(req, res){
+	res.send('The GET Request on "/" has been handled by the server.');
+})
+
+
+// router.use('/api', apiRouter);
+router.get('/basic1', function(req, res){
+	res.send('The GET Request on "/basic1" has been handled by the server.');
+    console.log("blahahahha")
+})
+
+router.get('/basic2', function(req, res){
+	res.send('The GET Request on "/basic2" has been handled by the server.');
+})
+
+router.get('/basic3', function(req, res){
+	res.send('The GET Request on "/basic3" has been handled by the server.');
+})
+
+
+// router.post('/', function(req, res){
+
+// 	console.log(req.body);
+// 	var username = req.body.username;
+// 	var password = req.body.password;
+
+// 	res.send("The username is: " + username + " and the password is: " + password);
+
+// })
+
+module.exports = router;
